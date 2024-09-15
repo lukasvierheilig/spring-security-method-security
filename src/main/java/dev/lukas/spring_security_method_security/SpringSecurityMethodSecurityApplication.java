16233489944dc3b36833c89d3a11ee39d1d56773
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-15 17:02:33 +0200

    Bootstrap project with initial dependencies

---
+++ src/main/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.lukas.spring_security_method_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMethodSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMethodSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lukas.spring_security_method_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMethodSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
