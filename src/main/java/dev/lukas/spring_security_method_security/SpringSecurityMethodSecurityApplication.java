c2764317964119b545e6f5cca8da08857703d312
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-15 21:15:54 +0200

    Add endpoints with role authorization

--- src/main/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplication.java
+++ src/main/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplication.java
@@ -3,37 +3,74 @@ package dev.lukas.spring_security_method_security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/greet")
+@EnableMethodSecurity
 public class SpringSecurityMethodSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityMethodSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMethodSecurityApplication.class, args);
+    }
+
+    @GetMapping("/public")
+    public String index() {
+        return "Hello World!";
+    }
 
 
-	@GetMapping()
-	public String greet() {
-		return "Hello World";
-	}
+    @GetMapping("/greet")
+    @PreAuthorize("hasAnyRole('USER')")
+    public String greet() {
+        return "Hello User";
+    }
 
-	@GetMapping("/secure")
-	public String secure() {
-		return "Stop there this is private!";
-	}
+    @GetMapping("/greet/secure")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String secure() {
+        return "Hello Admin";
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails anonymous = User.builder()
+                .username("anonymous")
+                .password("{noop}password")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password").
+                roles("USER", "ADMIN").build();
+        return new InMemoryUserDetailsManager(admin, anonymous, user);
+    }
 
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public").permitAll()
+                .requestMatchers("/greet").hasRole("USER")
+                .anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
 }
--- src/test/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplicationTests.java
+++ src/test/java/dev/lukas/spring_security_method_security/SpringSecurityMethodSecurityApplicationTests.java
@@ -1,13 +1,49 @@
 package dev.lukas.spring_security_method_security;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 @SpringBootTest
 class SpringSecurityMethodSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    SpringSecurityMethodSecurityApplication application;
+
+    @Test
+    void contextLoads() {
+        // Smoke test
+    }
+
+    @Test
+    void whenAnonymousUserRequestsIndexThenServeIndex() {
+        Assertions.assertThat(application.index()).isEqualTo("Hello World!");
+    }
+
+    @Test
+    @WithMockUser()
+    void whenUserRequestsGreetingThenGreetUser() {
+        Assertions.assertThat(application.greet()).isEqualTo("Hello User");
+    }
+
+    @Test
+    void whenNonUserRequestsGreetingThenRefuse() {
+        Assertions.assertThatException().isThrownBy(() -> application.greet());
+    }
+
+    @Test
+    void whenUserRequestsSecureGreetingThenRefuse() {
+        Assertions.assertThatException().isThrownBy(() -> application.secure());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void whenAdminRequestsSecureGreetingThenGreetAdmin() {
+        Assertions.assertThat(application.secure()).isEqualTo("Hello Admin");
+    }
+
 
 }
